9e2413fe161599cbd58f8610b504ec51a428389f
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-06-20 19:26:24 +0530

    Tailwind setup, universal styles and building components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import banner from "./assets/banner.png";
+import Card from "./components/Card";
 
 function App() {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div class="container m-auto">
+            <header>
+                <h1>Ninja Merch</h1>
+            </header>
+            <img class="rounded-md" src={banner} alt="site banner"/>
+
+            <div class="grid grid-cols-4 gap-10 my-4">
+
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+export default function Card() {
+    return(
+        <div class="bg-white p-4 text-center rounded-md shadow-ms">
+            <h2>Card Component</h2>
+            <p>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</p>
+            <button class="btn">Click me!</button>
+        </div>
+    );
+}
